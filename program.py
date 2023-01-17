ac249e95f701ff6426a4e7fa5f010ffd790a79e1
Charles2205 <cewudzie0@gmail.com>
2023-01-17 11:49:46 +0000

    Firsts Python Code

---
+++ program.py
@@ -0,0 +1,27 @@
+# first_name=input('Enter your first name: ')
+# last_name=input('Enter your last name: ')
+# print(first_name.upper())
+# print(last_name.lower())
+
+# email=input('Enter your email: ')
+# email_split=email.split('@')
+# print(name_split[0])
+
+
+# assignment
+# user_input = input("Enter an integer: ")
+# user_int = int(user_input)
+# print(user_int)
+
+# strip method
+# it is used to remove spaces at the begining and ending of a results the user types...
+# name=input('User Name: ')
+# print(name.strip())
+
+# Count 
+
+# length 
+first_name= input('Enter your first name: ')
+last_name= input('Enter your last name: ')
+print (len(first_name.strip()))
+print (len(last_name.strip()))
