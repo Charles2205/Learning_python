2c945bd0a973875cd0d893b9996c05c7ad4dd70a
Charles2205 <cewudzie0@gmail.com>
2023-01-23 22:30:24 +0000

    Assignment Added

---
+++ assignment.py
@@ -0,0 +1,31 @@
+# # assignment
+# user_input = input("Enter an integer: ")
+# user_int = int(user_input)
+# print(user_int)
+
+# Question 1
+# alphabet=input("Enter alphabet: ")
+# if alphabet in ['a','e','i','o','u']:
+#     print('Vowel')
+# else:print("consonant")
+
+# Question  2
+user_number=int((input('Enter your number: ')))
+if user_number % 2 == 0:
+    print('is a prime number')
+else:print("The number you've entered is not a prime number")
+
+# Question 3
+year=int(input('Enter year: '))
+if year % 4 == 0 and year % 100!= 0 or year % 400 == 0:
+    print('Leap year')
+else:print('Not a leap year')
+
+text=str((input('Enter a word: ')))
+Palindrome_check=text[::-1]
+if text == Palindrome_check:
+    print('Palindrome')
+else:print('Not a palindrome')
+
+# Question 5
+
