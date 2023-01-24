226ffc341080f2c6af18f555de0128073779b311
Charles2205 <cewudzie0@gmail.com>
2023-01-24 11:50:52 +0000

    Loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+# for loops # while loops
+# Using while loop in python 
+
+counter = 1
+while counter <= 100:
+    print(counter)
+    counter += 1
+
+
+    # PROMPT A USER REPEATELY UNTILL  THEY ENTER A NUMBER GREATER THAN 100
