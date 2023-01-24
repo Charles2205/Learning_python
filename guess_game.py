07a9e879763c867dcef01d83037e660a8a8220e9
Charles2205 <cewudzie0@gmail.com>
2023-01-24 13:07:53 +0000

    A complete guessing game

---
+++ guess_game.py
@@ -0,0 +1,21 @@
+# adding the random module file into our project
+import random
+
+# generate a random from 1-100
+gen_number=random.randint(1,10)
+# count user guesses
+user_guess_count=0
+
+while True:
+# Accepting user guessed number
+    user_input=int((input('Enter your number: ')))
+# determing if user's guess is correct
+    if user_input==gen_number:
+        print('Congratulations !!! You guessed right')
+        break
+    elif user_input>gen_number:
+        print('Too high.Try again')
+    elif user_input<gen_number:
+        print('Too low.Try again')
+    user_guess_count+=1
+print(f'the number of wrong guesses is {user_guess_count}')
