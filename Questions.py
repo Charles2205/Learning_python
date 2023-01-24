dab0dbc5ef37d90e4ee8e5e4eafc1691fff5fce5
Charles2205 <cewudzie0@gmail.com>
2023-01-24 12:27:46 +0000

    Guess Game

---
+++ Questions.py
@@ -0,0 +1,9 @@
+# Create a program that generates a random number btn 1 and 100 and repeatedly prompts the user to guess the number.The program should give a user feedback on
+# whether their guess is too high or too low and keep tracks the number of times the user guess.The program should end when the user enters the number correct 
+
+
+# While loop to calculate the fatoria of a number entered by the user 
+
+# Write a program that calculates the sum of all even numbers between 1 and the number entered  by the user using while loop 
+
+ 
--- loops.py
+++ loops.py
@@ -1,10 +1,16 @@
 # for loops # while loops
 # Using while loop in python 
-
-counter = 1
-while counter <= 100:
-    print(counter)
-    counter += 1
+# continue and break loops
 
 
+# counter = 1
+# while counter <= 100:
+#     print(counter)
+#     counter += 1
     # PROMPT A USER REPEATELY UNTILL  THEY ENTER A NUMBER GREATER THAN 100
+    # how to get user number
+    
+# while True:
+#         user_input=int((input('Enter a number: ')))
+#         if user_input >100:
+#             break
