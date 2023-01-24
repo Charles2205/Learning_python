a0c0c733aa63a4b1ae0fa16a45f628575a8d16b0
Charles2205 <cewudzie0@gmail.com>
2023-01-24 09:06:56 +0000

    Complete Assignment

--- assignment.py
+++ assignment.py
@@ -9,23 +9,41 @@
 #     print('Vowel')
 # else:print("consonant")
 
-# Question  2
-user_number=int((input('Enter your number: ')))
-if user_number % 2 == 0:
-    print('is a prime number')
-else:print("The number you've entered is not a prime number")
+# # Question  2
+# user_number=int((input('Enter your number: ')))
+# if user_number % 2 == 0:
+#     print('is a prime number')
+# else:print("The number you've entered is not a prime number")
 
-# Question 3
-year=int(input('Enter year: '))
-if year % 4 == 0 and year % 100!= 0 or year % 400 == 0:
-    print('Leap year')
-else:print('Not a leap year')
+# # Question 3
+# year=int(input('Enter year: '))
+# if year % 4 == 0 and year % 100!= 0 or year % 400 == 0:
+#     print('Leap year')
+# else:print('Not a leap year')
 
-text=str((input('Enter a word: ')))
-Palindrome_check=text[::-1]
-if text == Palindrome_check:
-    print('Palindrome')
-else:print('Not a palindrome')
+# text=str((input('Enter a word: ')))
+# Palindrome_check=text[::-1]
+# if text == Palindrome_check:
+#     print('Palindrome')
+# else:print('Not a palindrome')
 
-# Question 5
+# # Question 5
+# fahrenheit=float((input('Enter the temperature: ')))
+# celsius =(fahrenheit-32)*5/9
+# if fahrenheit<32:
+#     print('freezing')
+# elif fahrenheit>212:
+#     print('boiling')
+# else:
+#     print('temperature in celsius')
 
+#     #Question 6
+# password =str(input('Enter your password'))
+# if len('password') >8:
+#     password.search=str("[a-z]")
+#     password.serach=str("[A-Z]")
+#     password.search=int("[0-9]")
+#     print('password accepted')
+
+# else:
+#     print('password not accepted') 
