18f9576b113bf6d416327647da5c702fa9189149
Charles2205 <cewudzie0@gmail.com>
2023-01-25 12:59:55 +0000

    list in Python

---
+++ data_structure.py
@@ -0,0 +1,8 @@
+# List 
+# List are used to store different types of data values
+# They are organized and mutable
+
+# creating a list in python
+name=['joel',1,2,False,21]
+name[0]=("Charles")
+print(name)
