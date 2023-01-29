cc108d0327522cdf0d2bb2855d0a31b2d0fb533c
Charles2205 <cewudzie0@gmail.com>
2023-01-29 10:19:37 +0000

    Assignment almost done

--- data_structure.py
+++ data_structure.py
@@ -1,3 +1,4 @@
+
 # List 
 # List are used to store different types of data values
 # They are organized and mutable
@@ -6,3 +7,34 @@
 name=['joel',1,2,False,21]
 name[0]=("Charles")
 print(name)
+
+
+# Write a list of 10 numbers and use a  for loop to print each number 
+# Write a list of 10 numbers a use the bulit in sum function to find the sum of all the numbers 
+# Write a list of 10 numbers a use the bulit in min and max function to find the smallest and largest numbers in the list
+# Create a list of 10 random numbers and use a for loop to print the square of each number 
+# Write a list of  
+
+
+# Write a list of 10 numbers and use a  for loop to print each number
+person= ['Joel','Kofi Badu','John','Charles']
+for i in range(len(person)):
+    print(person[i])
+
+#  Write a list of 10 numbers a use the bulit in sum function to find the sum of all the numbers 
+num=[1,2,3,4,5,6,7,8,9,10]
+print(sum(num))
+
+# Write a list of 10 numbers a use the bulit in min and max function to find the smallest and largest numbers in the list
+num=[2,22,33,44,55,64,27,48,129,10]
+print(min(num))
+print(max(num))
+
+# Create a list of 10 random numbers and use a for loop to print the square of each number
+import random
+number =[]
+square=0
+for i in range(10):
+    number.append(random.randint(1,11))
+    square=square+number[i]
+print(square)
