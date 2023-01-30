417206e809d9246aa9807a6298b422fb464786fd
Charles2205 <cewudzie0@gmail.com>
2023-01-30 11:13:49 +0000

    Assignment done

--- data_structure.py
+++ data_structure.py
@@ -16,34 +16,25 @@
 # # Write a list of  
 
 
-# # Write a list of 10 numbers and use a  for loop to print each number
-# person= ['Joel','Kofi Badu','John','Charles']
-# for i in range(len(person)):
-#     print(person[i])
+# Write a list of 10 numbers and use a  for loop to print each number
+person= ['Joel','Kofi Badu','John','Charles']
+for names in range(len(person)):
+    print(person[names])
 
 # #  Write a list of 10 numbers a use the bulit in sum function to find the sum of all the numbers 
 # num=[1,2,3,4,5,6,7,8,9,10]
 # print(sum(num))
 
-# # Write a list of 10 numbers a use the bulit in min and max function to find the smallest and largest numbers in the list
+# Write a list of 10 numbers a use the bulit in min and max function to find the smallest and largest numbers in the list
 # num=[2,22,33,44,55,64,27,48,129,10]
 # print(min(num))
 # print(max(num))
 
-# Create a list of 10 random numbers and use a for loop to print the square of each number
+# # Create a list of 10 random numbers and use a for loop to print the square of each number
 # import random
 # number =[]
 # square=0
 # for i in range(0,10):
 #    number.append(random.randint(1,10)**2)    
-#    print(number)
+#    print(number[i])
 
-
-
-# import random
-# number =[1,2,3]
-# square=0
-# for i in range(0,2):
-# #    number.append(random.randint(1,10)**2) 
-#     square=square*number[i]
-#     print(square)   
--- guess_game.py
+++ guess_game.py
@@ -5,20 +5,26 @@ import random
 gen_number=random.randint(1,10)
 # count user guesses
 user_guess_count=0
-
+user_continue = False
 while True:
 # Accepting user guessed number
     user_input=int((input('Enter your number: ')))
 # determing if user's guess is correct
     if user_input==gen_number:
         print('Congratulations !!! You guessed right')
-        break
+        
     elif user_input>gen_number:
         print('Too high.Try again')
     elif user_input<gen_number:
         print('Too low.Try again')
-    if user_guess_count == 4:
+    if user_guess_count == 1234:
         print ('Reset ')
-        break
-    user_guess_count+= 1 
+        
+    user_guess_count+= 1
+    if user_input == gen_number:
+        print("Do you want to continue ? Type yes(1) or No(2)")
+
+        if user_input == 1:
+            print ("In test next level")
+
 print(f'the number of wrong guesses is {user_guess_count}')
