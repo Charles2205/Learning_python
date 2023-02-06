63e182eae66cf1ef2ec769e00e195f08fe8cf2b7
Charles2205 <cewudzie0@gmail.com>
2023-02-06 12:13:31 +0000

    Functions

---
+++ python_function.py
@@ -0,0 +1,25 @@
+# built in functions and user-defined functions
+# they are reuseable 
+
+# Why?
+# To break down your program into components and stop repeat codes
+
+# When?
+# when your codes turns to be plenty and unorganized we use function to break it down
+# pass is a keyword means nothing 
+#  How (Syntax)
+# def functionname (Parameters):
+''' docstring
+'''
+# instructions
+def sums():
+    """
+    This function helps user to add two numbers and display the results
+    """
+    x=2
+    y=3
+    results =x + y
+    return results
+
+    
+    
