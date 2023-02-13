128f933726b3d90ec4ea6ae73617e8dd4e281540
Charles2205 <cewudzie0@gmail.com>
2023-02-13 12:09:50 +0000

    Almost done

--- modules.py
+++ modules.py
@@ -1,10 +1,20 @@
-# import random as rand
-# num =rand.shuffle(2,1)
-# print(num)
+# # import random as rand
+# # num =rand.shuffle(2,1)
+# # print(num)
 
-import calendar
-year =2023
-print(calendar.calendar(year,m=1))
+# import calendar
+# year =2023
+# print(calendar.calendar(year,m=1))
 
-# Assignment:Create a program to ask the user of the year and month of the calendar the user wants to see
+# # Assignment:Create a program to ask the user of the year and month of the calendar the user wants to see
+import calendar as cal 
+
+year=int(input('Enter the year you want to see: '))
+month=int(input('Enter the month you want to see: '))
+results=cal.calendar(year)
+
+print(results)
+
+# import datetime as dt
+# print(dt.date.today())
 
