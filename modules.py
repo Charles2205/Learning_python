ea5c276e444820344a10bda97c381b29936c7a81
Charles2205 <cewudzie0@gmail.com>
2023-02-13 11:43:26 +0000

    Assignment added

--- modules.py
+++ modules.py
@@ -1,3 +1,10 @@
 # import random as rand
 # num =rand.shuffle(2,1)
 # print(num)
+
+import calendar
+year =2023
+print(calendar.calendar(year,m=1))
+
+# Assignment:Create a program to ask the user of the year and month of the calendar the user wants to see
+
