5b2e1e993af21705675d8a92842e0b97cfbb6e7b
Charles2205 <cewudzie0@gmail.com>
2023-02-14 12:10:25 +0000

    A text message app

--- class.py
+++ class.py
@@ -1,1 +1,11 @@
-class Person
+class Person:
+    """This is a class to represent a person object"""
+    # defining the attributes of the person
+    first_name=''
+    last_name=''
+    age = 0
+
+
+
+person1=Person()
+print(person1)
