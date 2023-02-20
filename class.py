f1a2070d21489067f45618c4e795ca2e23244a30
Charles2205 <cewudzie0@gmail.com>
2023-02-20 12:05:53 +0000

    Classes

--- class.py
+++ class.py
@@ -1,11 +1,41 @@
-class Person:
+# class Person:
+#     """This is a class to represent a person object"""
+#     # defining the attributes of the person
+#     first_name=''
+#     last_name=''
+#     age = 0
+#     def move(self):
+#         print('You can move')
+#     def eat(self):
+#         print('You can eat!!!')
+# # Create object
+# person1=Person()
+# person1.first_name='Joel'
+# person1.last_name='Badu'
+# person1.age=56
+# print(person1.first_name,person1.last_name)
+
+
+# person2=Person()
+# person2.first_name='Kofi'
+# person2.last_name = 'Badu'
+# person2.age=33
+# print(person2.first_name,person2.last_name)
+
+
+
+class User:
     """This is a class to represent a person object"""
     # defining the attributes of the person
     first_name=''
     last_name=''
     age = 0
+    def __init__(self,fn,ln,age):
+        self.first_name=fn
+        self.last_name=ln
+        self.age=age
 
+    def hello(self):
+        print(self.first_name)
 
-
-person1=Person()
-print(person1)
+user1 =User('Stephen','Charles',20)
